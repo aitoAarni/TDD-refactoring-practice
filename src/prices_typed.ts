7e55131fb100c3984b636d2f207edc4fe45cfccc
aitoAarni <aarni.rechardt@gmail.com>
2025-02-03 14:55:12 +0100

    change

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -83,7 +83,7 @@ function createApp(database: Database) {
     return reduction;
   }
 
-  function isMonday(date: Date, temporalDate: Temporal.PlainDate | undefined = undefined) {
+  function isMonday(date: Date | undefined = undefined, temporalDate: Temporal.PlainDate | undefined = undefined) {
     return temporalDate?.dayOfWeek === 1;
   }
 
