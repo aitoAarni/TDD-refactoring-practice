7e66876ce4d605167683e1f3ccec8cc1e3050f91
aitoAarni <aarni.rechardt@gmail.com>
2025-02-03 15:15:09 +0100

    change

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -100,7 +100,8 @@ function createApp(database: Database) {
       ) {
         return true;
       }
-      if (temporalDate && temporalDate && temporalDate.year === holiday2.year && temporalDate.month === holiday2.month && temporalDate.day === holiday2.day ){
+      if (temporalDate && 
+        temporalDate && temporalDate.year === holiday2.year && temporalDate.month === holiday2.month && temporalDate.day === holiday2.day ){
         
       }
     }
