995b794ea2c898097102202f7aba1b23dd0d0b9d
aitoAarni <aarni.rechardt@gmail.com>
2025-02-03 14:23:38 +0100

    change

--- src/prices_typed.ts
+++ src/prices_typed.ts
@@ -31,7 +31,8 @@ function createApp(database: Database) {
     }
   }
   function parsePlainDate(plainDateString: string | undefined): Temporal.PlainDate | undefined {
-    return; }
+    return;
+  }
 
   function calculateCost(age: number | undefined, type: string, date: Date | undefined, baseCost: number) {
     if (type === "night") {
